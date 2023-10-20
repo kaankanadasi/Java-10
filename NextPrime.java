b896eac803a5288ab7275d0b885a4a57b4dd590a
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2023-10-21 00:48:01 +0300

    Create NextPrime.java

---
+++ NextPrime.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NextPrime {
+    // kodu yasamdım
+    public static void main(String[]args) {
+        Scanner scan = new Scanner(System.in);
+        for (int i=0; i<5; i++) {
+            int input = scan.nextInt();
+            boolean prime = true
+            while (prime) {
+                input ++;
+                if (input%2==0 && input%3==0 && input%5==0 && input%7==0 && input%11==0 && input%13==0 && input%17==0 && input%19==0 && input%23==0 && input%29==0 && input%31==0 && input%37==0  && input%41==0 && input%43==0) {
+                    boolean prime = false
+                }
+            }
+
+        }
+
+    }
+}
