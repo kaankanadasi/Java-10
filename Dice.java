d1a3f5ffe71a33eae843a3f51213eb52d491be95
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2023-11-20 10:03:06 +0300

    Create Dice.java

---
+++ Dice.java
@@ -0,0 +1,14 @@
+/**
+ * bir zar 20 kere atılcak (5 set). her 20likten en yüksek zar değeri bir listeye eklenecek 
+ * ve sonunda her 20lik setten en yüksek zar değeri print edilcek
+ * @author Kaan Kanadasi
+ * @since 20 Nov 2023
+ */
+
+class Dice { 
+int[] dice;
+
+// 20 kere (int)Math.random(0,7); bunu array'de storela sonra array'deki en büyük değeri bulmak için iterate et
+
+
+}
