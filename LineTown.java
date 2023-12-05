e7cd9b51bda5407f463afc1373e7f8de81018ced
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2023-12-05 10:37:30 +0300

    Update LineTown.java

--- LineTown.java
+++ LineTown.java
@@ -2,21 +2,38 @@ public class LineTown {
     public static void main(String []args) {
         int length = Integer.parseInt(args[0]);
         int[] arr = new int[length];
-        int iteration = 0;
-        for(int i = 1; i <= length; i++) {
-            int arrayInput = Integer.parseInt(args[i]);
-            arr[iteration] += arrayInput;
-            iteration++;
+        int swaps = 0;
+        
+        // array inputu
+        for(int i = 0; i < length; i++) {
+            arr[i] = Integer.parseInt(args[i + 1]);
         }
-        // find minimum integer in the array
-        for(int j = 0; j < length; j++) {
-            int min = Integer.MAX;
-            if( arr[j] < min ) {
-                min = arr[j];
-            } 
 
-        // swap that array with the
+        // swap 
+        while(!isIncreasingOrder(arr)) {
+            for(int k = 0; k < arr.length-1; k++) {
+                if(arr[k] > arr[k+1]) {
 
+                    swaps++;
+                }
+            }
         }
+
     }
+
+    // istenilen düzenin olup olmadığını kontrol ediyor
+    public boolean isIncreasingOrder(int[] arr) {
+        for(int n = 0; n < arr.length-1; i++) {
+            if (!(arr[n] < arr[n+1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int notConvertable(int[] arr) {
+        // checks if the array can be converted to increasing order
+        return -1;
+    }
+
 }
